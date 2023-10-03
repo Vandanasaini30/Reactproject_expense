d7041e92ea7cf3dd7747c30a0695470130a68c23
vandanasaini30 <vandanasaini3010@gmail.com>
2023-10-03 19:05:15 +0530

    adding Expenses

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from '../UI/Card';
+const Expenses = (props)=> {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.item[0].title}
+        amount={props.item[0].amount}
+        date={props.item[0].date}
+        location={props.item[0].location}
+      />
+      <ExpenseItem
+        title={props.item[1].title}
+        amount={props.item[1].amount}
+        date={props.item[1].date}
+        location={props.item[1].location}
+      />
+      <ExpenseItem
+        title={props.item[2].title}
+        amount={props.item[2].amount}
+        date={props.item[2].date}
+        location={props.item[2].location}
+      />
+      <ExpenseItem
+        title={props.item[3].title}
+        amount={props.item[3].amount}
+        date={props.item[3].date}
+        location={props.item[3].location}
+      />
+    </Card>
+  );
+}
+export default Expenses;
